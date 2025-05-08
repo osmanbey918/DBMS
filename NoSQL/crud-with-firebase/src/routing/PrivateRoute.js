72dcfa1dde5b849ee84afa5cf307f70cb5bcb255
Usman Khan <usman918khan@gmail.com>
2025-05-08 11:46:48 +0500

    (deleting-old)

--- NoSQL/crud-with-firebase/src/routing/PrivateRoute.js
+++
@@ -1,7 +0,0 @@
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-
-export default function PrivateRoute({children}) {
-    const user = useSelector((store) => store.auth.user);
-    return user ? children : <Navigate to="/signup" />;
-}
--- NoSQL/crud-with-firebase/src/routing/PublicRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-
-export default function PublicRoute({children}) {
-    const user = useSelector((state) => state.auth?.user);
-    return user ? <Navigate to="/" /> : children;
-}
